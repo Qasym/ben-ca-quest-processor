7f322933494b5b05c7ddce278545c53e35a37aee
Kasymzhan <kasymzhan.abdyldayev@gmail.com>
2024-11-11 22:30:32 +0900

    minor modifications for data handling

--- src/main/kotlin/com/kasymzhan/quest/processor/data/UserReward.kt
+++ src/main/kotlin/com/kasymzhan/quest/processor/data/UserReward.kt
@@ -1,5 +1,6 @@
 package com.kasymzhan.quest.processor.data
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
@@ -11,9 +12,18 @@ data class UserReward(
     val id: ObjectId,
     val userId: String,
     val questId: String,
-    val status: Status,
-    val dateReceived: Date
-)
+    @field:JsonProperty("status")  // Ensures proper deserialization
+    private var _status: Status,
+    var dateReceived: Date? = null,
+) {
+    val status: Status
+        get() = _status
+
+    fun receiveReward() {
+        _status = Status.CLAIMED
+        dateReceived = Date()
+    }
+}
 
 enum class Status(name: String) {
     CLAIMED("CLAIMED"),
